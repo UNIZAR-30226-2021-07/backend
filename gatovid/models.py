6150199695211429bace6a2d93c8e63e007a6ee7
Mario Ortiz Manero <marioortizmanero@gmail.com>
2021-03-19 18:02:51 +0100

    base models for database testing

--- gatovid/models.py
+++ gatovid/models.py
@@ -5,3 +5,65 @@ juegos.
 NOTE: si este fichero crece considerablemente se puede convertir en un
 directorio en vez de un único fichero.
 """
+
+from enum import Enum
+
+from gatovid.exts import db
+
+
+class User(db.Model):
+    """
+    """
+
+    id = db.Column(db.String, primary_key=True)
+    password = db.Column(db.String, nullable=True)
+
+
+class Stats(db.Model):
+    """
+    """
+
+
+class GameManager:
+    """
+    """
+
+
+class Game:
+    """
+    """
+
+
+class Player:
+    """
+    """
+
+
+class Card:
+    """
+    """
+
+
+class CardTypes(Enum):
+    """
+    """
+
+
+class CardColors(Enum):
+    """
+    """
+
+
+class Message:
+    """
+    """
+
+
+class Pictures(Enum):
+    """
+    """
+
+
+class Boards(Enum):
+    """
+    """

e96944c9076459081165647a25c92a6e979414a2
Mario Ortiz Manero <marioortizmanero@gmail.com>
2021-03-24 20:02:19 +0100

    cleanup heroku script, ready for review

--- setup-heroku.sh
+++ setup-heroku.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
 # Script needed to configure the Heroku environment with `heroku.yml`.
 
-# heroku.yml doesn't support submodules, apparently
-echo ">> Cloning dependencies"
-echo ">>>>>> /usr/src/app/gatovid"
-ls /usr/src/app/gatovid
-echo ">>>>>> /usr/src/app/gatovid/assets"
-ls /usr/src/app/gatovid/assets
-echo ">>>>>> done"
-
-# Also gatovid-specific processes
+# Gatovid-specific processes
 echo ">> Setting up database"
 python -m gatovid --reset-db
 python -m gatovid --create-db

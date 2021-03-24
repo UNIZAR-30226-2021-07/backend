321fd190b66ab3b3cee68b6af6066423de857dca
Mario Ortiz Manero <marioortizmanero@gmail.com>
2021-03-24 19:25:22 +0100

    final fix

--- setup-heroku.sh
+++ setup-heroku.sh
@@ -5,9 +5,8 @@
 echo ">> Cloning dependencies"
 git init
 ./setup-submodules.sh
-ls /usr/src/app/gatovid
-ls /usr/src/app/gatovid/assets
 
 # Also gatovid-specific processes
 echo ">> Setting up database"
+python -m gatovid --reset-db
 python -m gatovid --create-db

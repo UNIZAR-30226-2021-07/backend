53525b0fd9256d5d304f6a2365e8d18d75c69673
Mario Ortiz Manero <marioortizmanero@gmail.com>
2021-03-24 19:47:00 +0100

    bump

--- setup-heroku.sh
+++ setup-heroku.sh
@@ -3,15 +3,11 @@
 
 # heroku.yml doesn't support submodules, apparently
 echo ">> Cloning dependencies"
-git init
-./setup-submodules.sh
 echo ">>>>>> /usr/src/app/gatovid"
 ls /usr/src/app/gatovid
 echo ">>>>>> /usr/src/app/gatovid/assets"
 ls /usr/src/app/gatovid/assets
 echo ">>>>>> done"
-git submodule init
-git submodule update
 
 # Also gatovid-specific processes
 echo ">> Setting up database"

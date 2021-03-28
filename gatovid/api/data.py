223307f691b928b51eff37436a48aaf0b10f2a7c
xzebra <zebrv.apps@gmail.com>
2021-03-28 15:37:25 +0200

    login working

--- gatovid/api/data.py
+++ gatovid/api/data.py
@@ -1,7 +1,11 @@
 """
 """
 
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 
 from gatovid.exts import db
 from gatovid.models import User
@@ -32,3 +36,30 @@ def test():
             "stats": str(user.stats),
         },
     }
+
+
+@mod.route("/login", methods=["GET", "POST"])
+def login():
+    email = request.args.get("email")
+    password = request.args.get("password")
+
+    if None in (email, password):
+        return {
+            "error": "Parámetro vacío",
+        }
+
+    # Comprobamos si existe un usuario con ese email
+    user = User.query.get(email)
+    if not user:
+        return {
+            "error": "El usuario no existe",
+        }
+
+    # Comprobamos si los hashes coinciden
+    if user.check_password(password) is False:
+        return {
+            "error": "Contraseña incorrecta",
+        }
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_Token=access_token)

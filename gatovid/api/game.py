5600037532881f011cb984248d622f4eb5e3842e
xzebra <zebrv.apps@gmail.com>
2021-03-29 18:19:31 +0200

    websockets auth

--- gatovid/api/game.py
+++ gatovid/api/game.py
@@ -2,10 +2,11 @@
 """
 
 from flask_socketio import emit
+from flask_jwt_extended import jwt_required
 
 from gatovid.exts import socket
 
-
 @socket.on("chat")
+@jwt_required()
 def chat(msg):
     emit("chat", msg)

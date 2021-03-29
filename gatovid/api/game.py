e7abd9e1df98d60e34c623ef4a54b15c759091e0
xzebra <zebrv.apps@gmail.com>
2021-03-29 19:47:03 +0200

    session check only on connect

--- gatovid/api/game.py
+++ gatovid/api/game.py
@@ -6,8 +6,22 @@ from flask_socketio import emit
 
 from gatovid.exts import socket
 
+@socket.on('connect')
+def connect():
+    """
+    Return False si queremos prohibir la conexión del usuario.
+    """
+    try:
+        # Comprobamos si el token es válido. Si el token es inválido,
+        # lanzará una excepción.
+        verify_jwt_in_request()
+    except:
+        print("user not connected")
+        emit("invalid token")
+        return False
 
+    print("user connected")
+    return True
 @socket.on("chat")
-@jwt_required()
 def chat(msg):
     emit("chat", msg)

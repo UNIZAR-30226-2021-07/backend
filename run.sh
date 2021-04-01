0e625f9ad264477463c5b026e729e1b809208c87
xzebra <zebrv.apps@gmail.com>
2021-04-01 23:02:30 +0200

    fix reload

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # Command ran for docker-compose and deployment
 
-extra_flags=
+extra_flags=""
 if [ "$DEBUG" = "True" ]; then
-    extra_flags+="--reload"
+    extra_flags="--reload"
 fi
 
 /usr/local/bin/gunicorn --worker-class eventlet -w 1 -b :$PORT 'gatovid.app:app' $extra_flags

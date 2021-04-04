b141b6d55f0a73666fd6933c1a916c20f461affe
xzebra <zebrv.apps@gmail.com>
2021-04-04 13:52:08 +0200

    temporary cors

--- gatovid/exts.py
+++ gatovid/exts.py
@@ -8,6 +8,7 @@ from flask_jwt_extended import JWTManager
 from flask_session import Session
 from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 # Base de datos
 db = SQLAlchemy()
@@ -19,3 +20,5 @@ jwt = JWTManager()
 socket = SocketIO(cors_allowed_origins="*")
 # Manager de sesiones para websocket
 sess = Session()
+# Cross Origins Requests
+cors = CORS()

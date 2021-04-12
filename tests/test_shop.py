a3bf2eeb7b0400fe164133fd7b7fe2ee02b04446
xzebra <zebrv.apps@gmail.com>
2021-04-12 19:13:25 +0200

    buy tests

---
+++ tests/test_shop.py
@@ -0,0 +1,49 @@
+"""
+Tests para las consultas de la tienda de objetos.
+"""
+
+from .base import GatovidTestClient
+
+
+class ShopTests(GatovidTestClient):
+    user_data = {
+        "email": "test_user1@gmail.com",
+        "password": "whatever1",
+    }
+
+    def test_buy(self):
+        resp = self.request_token(self.user_data)
+        token = resp.json["access_token"]
+
+        resp = self.request_shop_buy(item_id=3, item_type="board", token=token)
+        self.assertRequestOk(resp)
+
+        resp = self.request_shop_buy(item_id=3, item_type="profile_pic", token=token)
+        self.assertRequestOk(resp)
+
+    def test_wrong_input(self):
+        resp = self.request_token(self.user_data)
+        token = resp.json["access_token"]
+
+        resp = self.request_shop_buy(item_id=3, item_type="gato", token=token)
+        self.assertRequestErr(resp, 400)
+        resp = self.request_shop_buy(item_id=999, item_type="board", token=token)
+        self.assertRequestErr(resp, 400)
+        resp = self.request_shop_buy(item_id=-1, item_type="board", token=token)
+        self.assertRequestErr(resp, 400)
+
+    def test_wrong_input_types(self):
+        resp = self.request_token(self.user_data)
+        token = resp.json["access_token"]
+
+        resp = self.request_shop_buy(item_id="5", item_type="board", token=token)
+        self.assertRequestOk(resp)
+
+    def test_already_bought(self):
+        resp = self.request_token(self.user_data)
+        token = resp.json["access_token"]
+
+        resp = self.request_shop_buy(item_id="3", item_type="board", token=token)
+        self.assertRequestOk(resp)
+        resp = self.request_shop_buy(item_id="3", item_type="board", token=token)
+        self.assertRequestErr(resp, 400)

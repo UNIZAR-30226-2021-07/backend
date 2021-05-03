eb26a075a2e85177f26de3c023ca67bf29b27e38
xzebra <zebrv.apps@gmail.com>
2021-05-03 14:57:07 +0200

    test resume constraint

--- tests/test_game.py
+++ tests/test_game.py
@@ -148,13 +148,10 @@ class GameTest(WsTestClient):
 
     def test_pause(self):
         """
-        TODO: Modificar este test cuando se implemente el sistema de turnos.
+        Cualquier usuario de la partida podrá realizar una pausa. En ese momento
+        la partida quedará pausada para todos los jugadores de la misma.
         """
         clients, code = self.create_game()
-
-        # REQ: Cualquier usuario de la partida podrá realizar una pausa.
-        # Independientemente del turno, deberíamos ser capaces de pausar con
-        # cualquiera de los clientes.
 
         for (i, client) in enumerate(clients):
             # Ignoramos los eventos anteriores
@@ -175,3 +172,46 @@ class GameTest(WsTestClient):
             callback_args = client.emit("pause_game", False, callback=True)
             self.assertNotIn("error", callback_args)
 
+    def test_resume(self):
+        """
+        El usuario que realiza la pausa es el único capaz de volver a
+        reanudarla.
+        """
+        clients, code = self.create_game()
+
+        # Ignoramos los eventos anteriores
+        _ = clients[0].get_received()
+        _ = clients[1].get_received()
+        _ = clients[2].get_received()
+
+        # Pausamos con el cliente 0
+        callback_args = clients[0].emit("pause_game", True, callback=True)
+        self.assertNotIn("error", callback_args)
+
+        # Otro jugador espera recibir pausa
+        received = clients[1].get_received()
+        _, args = self.get_msg_in_received(received, "game_update", json=True)
+        self.assertIn("paused", args)
+        self.assertEqual(args["paused"], True)
+        self.assertIn("paused_by", args)
+        self.assertEqual(args["paused_by"], GENERIC_USERS_NAME.format(0))
+
+        # Intentamos reanudar con el cliente 2
+        callback_args = clients[2].emit("pause_game", False, callback=True)
+        self.assertIn("error", callback_args)
+
+        # Otro jugador no espera recibir reanudacion
+        received = clients[1].get_received()
+        self.assertEqual(received, [])
+            
+        # Reanudamos con el cliente 0
+        callback_args = clients[0].emit("pause_game", False, callback=True)
+        self.assertNotIn("error", callback_args)
+
+        # Otro jugador espera recibir reanudacion
+        received = clients[1].get_received()
+        _, args = self.get_msg_in_received(received, "game_update", json=True)
+        self.assertIn("paused", args)
+        self.assertEqual(args["paused"], False)
+        self.assertIn("paused_by", args)
+        self.assertEqual(args["paused_by"], GENERIC_USERS_NAME.format(0))

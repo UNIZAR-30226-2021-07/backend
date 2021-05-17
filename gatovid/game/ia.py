82cdf4c300517de7907415964b0310b5afa8c12a
xzebra <zebrv.apps@gmail.com>
2021-05-17 13:50:11 +0200

    change ai structure

--- gatovid/game/ia.py
+++ gatovid/game/ia.py
@@ -11,22 +11,36 @@ from gatovid.game.cards import Color, Treatment, Medicine, Organ
 if TYPE_CHECKING:
     from gatovid.game import Game, Player
 
-class IA:
-    player: "Player"
+def next_action(player: "Player", game: "Game") -> [Action]:
+    """
+    Devuelve la acción a realizar por la IA.
+    """
 
-    def __init__(self, player: "Player"):
-        self.player = player
+    # Prioridad de las acciones:
+    # 0. Algunos tratamientos especiales
+    # 1. Curar tus propios órganos
+    # 2. Proteger / inmunizar tus propios órganos
+    # 3. Colocar órganos
+    # 4. Infectar / extirpar órganos ajenos
 
-    def next_action(self, game: "Game") -> Action:
-        """
-        Devuelve la acción a realizar por la IA.
-        """
+    actions_priority = [action_special_treatments, action_heal_self]
+    for action_func in actions_priority:
+        action = action_func(player, game)
+        if action is not None:
+            return action
 
-        # Prioridad de las acciones:
-        # 1. Curar tus propios órganos
-        # 2. Proteger / inmunizar tus propios órganos
-        # 3. Colocar órganos
-        # 4. Infectar / extirpar órganos ajenos
+    # TODO: Descartar cartas que no se vayan a usar
+    # return Discard({ "slot": medicine_idx })
+def organs_to_heal(player: "Player") -> [int]:
+    """
+    Devuelve una lista de slots de pilas que requieren curación.
+    """
 
-    def find_card_type(self, card_type: str) -> [int]:
-        """ """
+    is_infected = lambda i,p: p.is_infected()
+    get_index = lambda i,p : i
+
+    infected = filter(is_infected, enumerate(player.body.piles))
+    return list(map(get_index, infected))
+
+def find_card_type(card_type: str, treatment_type: str = None) -> [int]:
+    """ """

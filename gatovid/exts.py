7547de248f5bab498206da51e12542fba9f9d25c
xzebra <zebrv.apps@gmail.com>
2021-05-21 13:45:01 +0200

    format

--- gatovid/exts.py
+++ gatovid/exts.py
@@ -11,9 +11,11 @@ from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
 
 # Base de datos
-db = SQLAlchemy(session_options={
-    "expire_on_commit": True,
-})
+db = SQLAlchemy(
+    session_options={
+        "expire_on_commit": True,
+    }
+)
 # Para el encriptado de datos
 bcrypt = Bcrypt()
 # Manager de sesiones con JWT
